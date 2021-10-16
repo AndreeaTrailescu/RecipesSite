7bd64cf993d2ea64f858f164ba94544565da9454
Andreea Trailescu <andreeatrailescu@gmail.com>
2021-10-16 15:25:08 +0300

    finish style

--- client/src/components/RecipeDetails/CommentSection.jsx
+++ client/src/components/RecipeDetails/CommentSection.jsx
@@ -27,9 +27,9 @@ const CommentSection = ({ recipe }) => {
         <div>
             <div className={classes.commentsOuterContainer}>
                 <div className={classes.commentsInnerContainer}>
-                    <Typography gutterBottom variant="h6">Comments</Typography>
+                    <Typography className={classes.section} gutterBottom variant="h6">Comments</Typography>
                     {comments.map((c, i) => (
-                        <Typography key={i} gutterBottom variant="subtitle1">
+                        <Typography className={classes.section} key={i} gutterBottom variant="subtitle1">
                         <strong> {c.split(': ')[0]}</strong>
                         {c.split(':')[1]}
                         </Typography>
@@ -38,8 +38,9 @@ const CommentSection = ({ recipe }) => {
                 </div>
                 {user?.result?.name && (
                     <div style={{ width: '70%' }}>
-                        <Typography gutterBottom variant="h6">Write a comment:</Typography>
+                        <Typography className={classes.section} gutterBottom variant="h6">Write a comment:</Typography>
                         <TextField 
+                            className={classes.root}
                             fullWidth
                             rows={4}
                             variant="filled"
@@ -48,7 +49,7 @@ const CommentSection = ({ recipe }) => {
                             value={comment}
                             onChange={(e) => setComment(e.target.value)}
                         />
-                        <Button style={{ marginTop: '10px' }} fullWidth disabled={!comment} variant="contained" onClick={handleClick}>
+                        <Button className={classes.commentButton} style={{ marginTop: '10px' }} fullWidth disabled={!comment} variant="contained" onClick={handleClick}>
                             Comment
                         </Button>
                     </div>
--- client/src/components/RecipeDetails/styles.js
+++ client/src/components/RecipeDetails/styles.js
@@ -20,6 +20,7 @@ export default makeStyles((theme) => ({
     borderRadius: '20px',
     margin: '10px',
     flex: 1,
+    fontFamily: ['curves',].join(','),
   },
   imageSection: {
     marginLeft: '20px',
@@ -33,6 +34,8 @@ export default makeStyles((theme) => ({
     [theme.breakpoints.down('sm')]: {
       flexDirection: 'column',
     },
+    background: 'linear-gradient(0deg, #f1dabf 70%, #92817a 120%)',
+    borderRadius: '10px',
   },
   loadingPaper: {
     display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px', borderRadius: '15px', height: '39vh',
@@ -40,10 +43,30 @@ export default makeStyles((theme) => ({
   commentsOuterContainer: {
     display: 'flex',
     justifyContent: 'space-between',
+    borderRadius: '10',
   },
   commentsInnerContainer: {
     height: '200px',
     overflowY: 'auto',
     marginRight: '30px',
-  }
+  },
+  recommendedImage: {
+    borderRadius: '30%',
+    margin: '10px 10px 0 -12px',
+  },
+  commentButton: {
+    background: 'linear-gradient(0deg, #f1dabf 70%, #92817a 120%)',
+    border: 0,
+    borderRadius: 6,
+    boxShadow: '1px 1px 2px 2px #04030f',
+    color: '#04030f',
+    height: 48,
+  },
+  root: {
+    '& label.Mui-focused': {
+      color: 'black',
+      fontFamily: ['curves',].join(','),
+      fontWeight: 100,
+    },
+  },
 }));
